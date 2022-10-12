8953370b381878534695c9bc169b9448249e8b81
AbhishekKmrSingh <91712759+AbhishekKmrSingh@users.noreply.github.com>
2022-10-13 01:06:47 +0530

    Create listtoarray.java

---
+++ listtoarray.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList languages = new ArrayList<>();
+
+    // Add elements in the list
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("ArrayList: " + languages);
+
+    // Create a new array of String type
+    String[] arr = new String[languages.size()];
+
+    // help to convert list to array
+   
+    }
+  }
+}
